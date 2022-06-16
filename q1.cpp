0de8834335deeafd5bb9e6cbb2942776c9d11b44
Arsh Khan <91742287+arsh9873@users.noreply.github.com>
2022-06-17 00:19:11 +0400

    still doesn't work properly always
    still doesn't work properly always but sometimes it does for simple cases and not edge cases.

--- q1.cpp
+++ q1.cpp
@@ -1,30 +1,45 @@
 // Example program {1,2,3,4,5,5,6}={1,2,3,4,5,6,7},distinct and new one
 //should be bigger than the other
 #include <iostream>
-#include <string>
 using namespace std;
+struct arr{
+int value=0;
+int pos=-1;
+}a[20];
 int main()
 {
-int a[7],pos[7],k=0;
-for(int i=0;i<7;i++)
+int size;
+cout<<"\nenter the number of your entries";
+cin>>size;
+cout<<"\nenter the elements";
+for(int i=0;i<size;i++)
 {
-    cin>>a[i];
-    pos[i]=-1;
+cin>>a[i].value;
+a[i].pos=i;
+}
+for(int i=0;i<size;i++)
+	for(int j=i+1;j<size;j++)
+		{	int temp,temp2;;
+			if(a[i].value>a[j].value)
+				{
+				temp=a[j].value;
+				temp2=a[j].pos;
+				a[j].value=a[i].value;	
+				a[j].pos=a[i].pos;
+				a[i].value=temp;
+				a[i].pos=temp2;
+				}
+			}
+for(int i=0;i<size;i++)
+	{
+		for(int j=i+1;j<size;j++)
+			{
+			if(a[i].value==a[j].value)
+			a[j].value++;
+			}
+		}
+for(int i=0;i<size;i++)
+cout<<"-"<<a[a[a[i].pos].pos].value;		
+
 }
 
-for(int i=0;i<7;i++)
-    {
-        for(int j=i+1;j<7;j++)
-            {   
-            if(a[i]==a[j])
-                {
-                a[j]=a[j]+1;    
-                }    
-            }
-       
-    }
-for(int i=0;i<7;i++)
-{
-cout<<"-"<<a[i];
-}
-}
