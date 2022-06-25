cb27f1f8029ddaa02d4d8f3ed8970ade58578b2f
Arsh Khan <91742287+arsh9873@users.noreply.github.com>
2022-06-25 22:14:42 +0400

    updated q9
    negative number runs and infinite loop fixed

--- q9.cpp
+++ q9.cpp
@@ -1,36 +1,35 @@
-// Example program
-#include <iostream>
-#include <string>
-using namespace std;
-int triangle(int n){
-    if(n==0)
-    return 0;
-    return n+triangle(n-1);
-}
-int tripyramid(int n){//1,4,10,20..
-if(n==0)
-return 0;
-return triangle(n)+tripyramid(n-1);
-}
-int main()
-{
-int opt,num=0;
-char ch;
-do{
-cout<<"\n1-triangular numbers \n-2 pyrimad triangular numbers";;
-cin>>opt;
-cout<<"enter the number in list";
-cin>>num;
-switch (opt){
-    case 1:cout<<triangle(num);
-            break;
-    case 2:cout<<tripyramid(num);
-            break;
-    default:cout<<"enter valid choice";
-            break;
-}
-cout<<"\ncontinue (y/n)";
-cin>>ch;
-}while(ch=='y');
-}
+	#include <iostream>
+	#include <string>
+	using namespace std;
+	int triangle(int n){
+		if(n==0 || n<0)
+		return 0;
+		return n+triangle(n-1);
+	}
+	int tripyramid(int n){//1,4,10,20..
+	if(n==0 || n<0)
+	return 0;
+	return triangle(n)+tripyramid(n-1);
+	}
+	int main()
+	{
+	int opt,num=0;
+	char ch;
+	do{
+	cout<<"\n1-triangular numbers \n-2 pyrimad triangular numbers";;
+	cin>>opt;
+	cout<<"enter the number in list";
+	cin>>num;
+	switch (opt){
+		case 1:cout<<triangle(num);
+				break;
+		case 2:cout<<tripyramid(num);
+				break;
+		default:cout<<"enter valid choice";
+				break;
+	}
+	cout<<"\ncontinue (y/n)";
+	cin>>ch;
+	}while(ch=='y');
+	}
 
